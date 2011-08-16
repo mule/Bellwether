0bc804591e5d3edaa4e58adddb96368e632d9130
Jukka Puranen <jukka.puranen@gmail.com>
2011-08-16 16:20:22 +0300

    inital commit

---
+++ BellWether/Areas/Admin/Controllers/BellAdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BellWether.Areas.Admin.Controllers
+{
+    public class BellAdminController : Controller
+    {
+
+
+
+
+        //
+        // GET: /Admin/BellAdmin/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Admin/BellAdmin/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index", new { area = "Admin", controller = "Home"} );
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+
+
+
+
+
+
+    }
+}
---
+++ BellWether/Models/Bell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BellWether.Models {
+  public class Bell {
+    [Required()]
+    public string Name { get; set; }
+  }
+}
